093fbd2f4254818ccd9aaebbddf6074bad429bab
Feju <felipe_julio@live.com>
2025-04-27 00:53:59 -0300

    feat: fix next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "/ff12-font-editor",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
